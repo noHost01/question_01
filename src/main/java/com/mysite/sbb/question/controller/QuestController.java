3d67795fc7c41f4d5220227255ce982cd20ddef2
noHost01 <ljh12081308@naver.com>
2022-07-14 11:54:44 +0900

    question 질문상세

--- src/main/java/com/mysite/sbb/question/controller/QuestController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestController.java
@@ -1,12 +1,12 @@
 package com.mysite.sbb.question.controller;
 
-import com.mysite.sbb.question.dao.QuestRepository;
 import com.mysite.sbb.question.domain.Question;
 import com.mysite.sbb.question.service.QuestionService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,5 +24,12 @@ public class QuestController {
         List<Question> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showDetail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/com/mysite/sbb/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/service/QuestionService.java
@@ -2,10 +2,12 @@ package com.mysite.sbb.question.service;
 
 import com.mysite.sbb.question.dao.QuestRepository;
 import com.mysite.sbb.question.domain.Question;
+import com.mysite.sbb.question.util.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -13,5 +15,14 @@ public class QuestionService {
     private QuestRepository questRepository;
     public List<Question> getList() {
         return questRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
