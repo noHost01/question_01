7b9f47679511decb805ed3641ea712480d809e03
noHost01 <ljh12081308@naver.com>
2022-07-18 09:52:59 +0900

    질문등록 URL찾기

--- src/main/java/com/mysite/sbb/question/controller/QuestController.java
+++ src/main/java/com/mysite/sbb/question/controller/QuestController.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,5 +32,9 @@ public class QuestController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+    @GetMapping("/create")
+    public String showCreate() {
+        return "question_form";
     }
 }
